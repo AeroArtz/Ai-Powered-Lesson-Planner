9b1a91051a91fcd889edff8f1e44b19546162f7d
AeroArtz <abdulrehmanikram9710@gmail.com>
2024-06-30 10:47:05 +0400

    first addition

---
+++ src/components/LessonContent.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  
+
+const LessonContent = ({items}) => {
+  return (
+    <div className='m-20 p-7 rounded-lg border border-gray-600'>
+        <Carousel>
+          <CarouselContent>
+            {items.map((elm) => 
+                <CarouselItem>
+                    <h4 className='text-white'>
+                      {elm}
+                    </h4>
+                </CarouselItem>
+            )}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+
+    </div>
+  )
+}
+
+export default LessonContent
