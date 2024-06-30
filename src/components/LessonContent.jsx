3ed45b4bc42ebc0edc7e2f24f35429a5e60875b9
AeroArtz <70429569+AeroArtz@users.noreply.github.com>
2024-06-30 13:37:54 +0400

    Update LessonContent.jsx

--- src/components/LessonContent.jsx
+++ src/components/LessonContent.jsx
@@ -9,15 +9,24 @@ import {
   
 
 const LessonContent = ({items}) => {
+  
   return (
-    <div className='m-20 p-7 rounded-lg border border-gray-600'>
+    <div className='m-20 p-7 rounded-lg border border-gray-800'>
         <Carousel>
           <CarouselContent>
             {items.map((elm) => 
                 <CarouselItem key={elm}>
-                    <h4 className='text-white'>
-                      {elm}
-                    </h4>
+                    <div className='flex flex-col space-y-4 items-center'>
+
+                      <h4 className='text-indigo-300 text-xl'>
+                        {elm.title}
+                      </h4>
+
+                      <h5 className='text-gray-100 text-sm'>
+                        {elm.content}
+                      </h5>
+
+                    </div>
                 </CarouselItem>
             )}
           </CarouselContent>
